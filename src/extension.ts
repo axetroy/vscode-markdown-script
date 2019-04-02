212cd029adf5fddde05d5f5be55aa390a11af088
axetroy <troy450409405@gmail.com>
2019-04-02 15:38:16 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,20 @@
 // Import the module and reference it with the alias vscode in your code below
 
 import * as vscode from "vscode";
+import * as _ from "lodash";
+import { init, localize } from "vscode-nls-i18n";
 const processExists = require("process-exists");
 const remark: any = require("remark");
-import * as _ from "lodash";
 
 enum Shell {
   Shell = "shell",
   Sh = "sh",
   Bash = "bash"
+}
+
+enum Command {
+  Copy = "markdown-script.copy",
+  Run = "markdown-script.run"
 }
 
 const shellLanguages = {
@@ -23,10 +29,10 @@ const shellLanguages = {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  init(context);
   class ShellCodeLensProvider implements vscode.CodeLensProvider {
     public async provideCodeLenses(
-      document: vscode.TextDocument,
-      token: vscode.CancellationToken
+      document: vscode.TextDocument
     ): Promise<vscode.CodeLens[]> {
       const ast = remark()
         .data("setting", {
@@ -41,40 +47,49 @@ export function activate(context: vscode.ExtensionContext) {
       );
 
       const array = codeNodes.map((v: any) => {
-        // a zero-base line number
+        // !!!: vscode 是从第 0 行开始, 而解析器从第 1 行开始
         const startLineNum = v.position.start.line - 1;
         const endLineNum = v.position.end.line - 1;
-        const scriptStartLineNum = startLineNum + 1;
-        const scriptEndLineNum = endLineNum;
+
+        const start = new vscode.Position(v.position.start.line, 0);
+
+        const end = new vscode.Position(
+          endLineNum - 1,
+          document.lineAt(endLineNum - 1).text.length
+        );
 
         const script: string = document
-          .getText(new vscode.Range(scriptStartLineNum, 0, scriptEndLineNum, 0))
+          .getText(new vscode.Range(start, end))
           .trim();
 
-        return [
+        const copy = [
           new vscode.CodeLens(
-            new vscode.Range(startLineNum, 0, endLineNum, 0),
+            new vscode.Range(startLineNum, 0, endLineNum, 3),
             {
-              title: "Click to copy",
-              command: "mdscript.copy",
-              arguments: [scriptStartLineNum, scriptEndLineNum]
+              title: localize("ext.btn.click2copy"), // click to copy
+              command: Command.Copy,
+              arguments: [start, end]
             }
           )
-        ].concat(
+        ];
+
+        const run =
           v.lang in shellLanguages
             ? [
                 new vscode.CodeLens(
-                  new vscode.Range(startLineNum, 0, endLineNum, 0),
+                  new vscode.Range(startLineNum, 0, endLineNum, 3),
                   {
-                    title: "Click to run",
-                    command: "mdscript.run",
+                    title: localize("ext.btn.click2run"), // click to run
+                    command: Command.Run,
                     arguments: [script]
                   }
                 )
               ]
-            : []
-        );
+            : [];
+
+        return [...copy, ...run];
       });
+
       return _.flatten(array);
     }
   }
@@ -89,7 +104,7 @@ export function activate(context: vscode.ExtensionContext) {
   let terminal: vscode.Terminal | null = null;
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("mdscript.run", async (script: string) => {
+    vscode.commands.registerCommand(Command.Run, async (script: string) => {
       const terminalName = "Markdown Script";
       if (!terminal) {
         terminal = vscode.window.createTerminal(terminalName);
@@ -110,18 +125,13 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "mdscript.copy",
-      async (scriptStartLineNum: number, scriptEndLineNum: number) => {
+      Command.Copy,
+      async (start: vscode.Position, end: vscode.Position) => {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
           return [];
         }
-        editor.selection = new vscode.Selection(
-          scriptStartLineNum,
-          0,
-          scriptEndLineNum,
-          0
-        );
+        editor.selection = new vscode.Selection(start, end);
         vscode.commands.executeCommand("editor.action.clipboardCopyAction");
       }
     )
